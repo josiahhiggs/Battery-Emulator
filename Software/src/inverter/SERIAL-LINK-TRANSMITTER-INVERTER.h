de842f5846f558413582aa7307d497542a2b465b
josiahhiggs <79869367+josiahhiggs@users.noreply.github.com>
2025-06-15 19:54:37 +1200

    update

---
+++ Software/src/inverter/SERIAL-LINK-TRANSMITTER-INVERTER.h
@@ -0,0 +1,11 @@
+#ifndef SERIAL_LINK_TRANSMITTER_INVERTER_H
+#define SERIAL_LINK_TRANSMITTER_INVERTER_H
+
+#include <Arduino.h>
+#include "../include.h"
+#include "../lib/mackelec-SerialDataLink/SerialDataLink.h"
+
+void manageSerialLinkTransmitter();
+void setup_inverter(void);
+
+#endif
